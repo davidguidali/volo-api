8104a6331bcee0b7d27aef00a4d56b769db29f95
dguidali <dguidali@greenliff.com>
2020-08-02 11:17:47 +0200

    Added json validation

---
+++ source/Volo.Api/Controllers/Opcua/DatapointMessage.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Volo.Api.Controllers.Opcua
+{
+    public class DatapointMessage
+    {
+        [Required]
+        public string Identifier { get; set; }
+
+        public int Value { get; set; }
+    }
+}
--- source/Volo.Api/Controllers/Opcua/OpcuaController.cs
+++ source/Volo.Api/Controllers/Opcua/OpcuaController.cs
@@ -1,33 +1,32 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using Grpc.Core;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Volo.Api.Controllers.Opcua;
 
 namespace Volo.Api.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
-    public class DatapointController : ControllerBase
+    [Route("opcua/datapoint")]
+    public class OpcuaController : ControllerBase
     {
-        private readonly ILogger<DatapointController> _logger;
+        private readonly ILogger<OpcuaController> _logger;
         private readonly IConfiguration _configuration;
 
-        public DatapointController(ILogger<DatapointController> logger, IConfiguration configuration)
+        public OpcuaController(ILogger<OpcuaController> logger, IConfiguration configuration)
         {
             _logger = logger;
             _configuration = configuration;
         }
 
         [HttpPost]
-        public async Task<OpcuaControllerResult> Post(OpcuaControllerMessage message)
+        public async Task<OpcuaControllerResult> Post(DatapointMessage message)
         {
             var channel = CreateChannel();
             var client = new OpcuaControllerService.OpcuaControllerServiceClient(channel);
-            var result = await client.SetDatapointAsync(message, new CallOptions().WithWaitForReady().WithDeadline(DateTime.UtcNow.AddSeconds(20)));
+            var result = await client.SetDatapointAsync(new OpcuaControllerMessage { Identifier = message.Identifier, Value = message.Value }, new CallOptions().WithWaitForReady().WithDeadline(DateTime.UtcNow.AddSeconds(20)));
 
             return result;
         }
